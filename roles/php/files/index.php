05ef697edc9510ba86af27bd7ba998f188ce6046
Rodrigo Bossini <professorbossini@gmail.com>
2020-04-29 00:47:07 -0300

    Configuração do Ansible. Lidando com inventários dinâmicos

---
+++ roles/php/files/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Devops na nuvem da Amazon!</title>
+</head>
+
+<body>
+    <p>
+        <?php echo "Hora atual: " .date("h:i:sa"); ?>
+    </p>
+
+</body>
+
+</html>
